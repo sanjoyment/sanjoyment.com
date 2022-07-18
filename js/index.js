cc3e5f40ae93d450ddc480a90d99a6068c066989
Sanjay Suthar <sanjaymsuthar@gmail.com>
2022-07-18 22:37:56 +0530

    date extended

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 (function() {
-    var target_date = new Date("July 20, 2022").getTime();
+    var target_date = new Date("July 22, 2022").getTime();
 
     var days, hours, minutes, seconds, countdown = document.getElementById("countdown");
 
